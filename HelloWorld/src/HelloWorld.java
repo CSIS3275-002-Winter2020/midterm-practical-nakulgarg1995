77ffd04f6555623691287e33ff07c3d13b9b1b31
nakulgarg1995 <gargn1@student.douglascollege.ca>
2020-02-14 13:23:10 -0800

    ADDING EMAIL

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Nakul Garg");
+		System.out.println("Nakul Garg ");
 		System.out.println("gargn1@student.douglascollege.ca");
 
 	}
